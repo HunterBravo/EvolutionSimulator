39a44020d5c06e6d53a5ef99cc3a03bfefea3863
brian <brianwinter@outlook.com>
2024-05-01 15:03:15 +0100

    added food eating noises

--- Scenes/Food.gd
+++ Scenes/Food.gd
@@ -1,6 +1,7 @@
 extends Area3D
 
 signal eat(food : Object,eater : Object)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,4 +16,8 @@ func _on_body_entered(body):
 	print("hot body?")
 	print(body.name)
 	if body.name == "SearchAI":
+		$AudioStreamPlayer.play()
+		await get_tree().create_timer(0.83).timeout
 		eat.emit(self,body)
+		
+		
